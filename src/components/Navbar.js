ffdf804795627f13866cc1e1b9062e5badc4a2bf
Mohamedessid <100768466+Mohamedessid@users.noreply.github.com>
2022-05-23 00:41:26 +0100

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from "../images/logo.png";
 import "../css/navbar.css"
+/* react icons library*/
+import {RiCustomerService2Line} from "react-icons/ri";
+import {AiFillInfoCircle} from "react-icons/ai"
+import {Ri24HoursFill} from "react-icons/ri"
+import {AiFillQuestionCircle} from "react-icons/ai"
+
+
+
 class Navbar extends Component{
   render(){
   return (
@@ -10,10 +18,10 @@ class Navbar extends Component{
         <a href="#Home" class="logo"><img src={logo}/></a>
                 <nav class="navbar">
                     
-                    <a href="#about">about</a>
-                    <a href="#Services">Services</a>
-                    <a href="#Contact">Contact</a>
-                    <a href="#How">How it works</a>
+                    <a href="#about"><AiFillInfoCircle/> about</a>
+                    <a href="#Services"><Ri24HoursFill/> Services</a>
+                    <a href="#Contact"><RiCustomerService2Line/> Contact</a>
+                    <a href="#How"><AiFillQuestionCircle/> How it works</a>
                     <button className='btn'>Sign In</button>
                 </nav>
                 <div id="menu-btn" class="fas fa-bars"></div>
