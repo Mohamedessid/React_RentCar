5e755b2d37bc908de5842da72f760eeec147785e
Mohamedessid <100768466+Mohamedessid@users.noreply.github.com>
2022-05-26 09:53:22 +0100

    flex

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import '../css/SecondContent.css';
 import firstCar from "../images/first-car.jpg";
+import secondCar from "../images/seconcar.jpg"
 class SecondContent extends Component{
     render(){
         return(
@@ -11,7 +12,7 @@ class SecondContent extends Component{
                         <p>we Provide Our costumers with the most incrdible Driving emotions
 that's why we have multiple car's choices</p>
                     </div>
-                    <div className="flex">
+                    <div className="flex-two">
                         <div className="first-car">
                             <img src={firstCar}/>
                             <h4>Small and practical</h4>
@@ -23,32 +24,32 @@ consectetur adipiscing elit Lorem ipsum dolor sit</p>
                             </div>
                         </div>
 {/* second choice */}
-                    </div>
-                    <div className="flex">
-                        <div className="first-car">
-                            <img src={firstCar}/>
-                            <h4>Small and practical</h4>
+                    
+                    
+                        <div className="first-car mod">
+                            <img src={secondCar}/>
+                            <h4>Spacious</h4>
                             <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit 
 consectetur adipiscing elit Lorem ipsum dolor sit</p>
                             <div className="boxe">
-                         <h5>   <a href="#" > Start at 70Dt/day</a></h5>
+                         <h5>   <a href="#" > Start at 100Dt/day</a></h5>
                             <h6>+70 cars</h6>
                             </div>
                         </div>
 
-                    </div>
-                    <div className="flex">
+                    
+                    <div className="flex-two">
                         <div className="first-car">
                             <img src={firstCar}/>
-                            <h4>Small and practical</h4>
+                            <h4>Luxurious</h4>
                             <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit 
 consectetur adipiscing elit Lorem ipsum dolor sit</p>
                             <div className="boxe">
-                         <h5>   <a href="#" > Start at 70Dt/day</a></h5>
+                         <h5>   <a href="#" > Start at 200Dt/day</a></h5>
                             <h6>+70 cars</h6>
                             </div>
                         </div>
-
+                        </div>
                     </div>
                     
                 </section>
